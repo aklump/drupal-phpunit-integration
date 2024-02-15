67a0bb032f7e7c8e11b3b3657ebdca378b282c3a
Aaron Klump <sourcecode@intheloftstudios.com>
2024-02-14 17:14:16 -0800

    Fix bug with phpunit args when more than one

--- init/run_integration_tests.sh
+++ init/run_integration_tests.sh
@@ -13,7 +13,7 @@ for arg in "$@"; do
   fi
 done
 
-./vendor/bin/phpunit -c ./tests_integration/phpunit.xml "$phpunit_args"
-#./vendor/bin/phpunit -c ./tests_integration/phpunit.xml --testdox "$phpunit_args"
-#php -d xdebug.mode=coverage ./vendor/bin/phpunit -c ./tests_integration/phpunit.xml --coverage-html=./tests_integration/reports "$phpunit_args"
+./vendor/bin/phpunit -c ./tests_integration/phpunit.xml "${phpunit_args[@]}"
+#./vendor/bin/phpunit -c ./tests_integration/phpunit.xml --testdox "${phpunit_args[@]}"
+#php -d xdebug.mode=coverage ./vendor/bin/phpunit -c ./tests_integration/phpunit.xml --coverage-html=./tests_integration/reports "${phpunit_args[@]}"
 #echo "./tests_integration/reports/index.html"
