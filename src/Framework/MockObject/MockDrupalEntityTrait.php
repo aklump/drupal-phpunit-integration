c97143082b7db3736e6c36c2c65105c64e384918
Aaron Klump <sourcecode@intheloftstudios.com>
2024-03-28 10:31:59 -0700

    Add getEntity, getFieldDefinition to item list mock.

--- src/Framework/MockObject/MockDrupalEntityTrait.php
+++ src/Framework/MockObject/MockDrupalEntityTrait.php
@@ -6,6 +6,7 @@ namespace AKlump\Drupal\PHPUnit\Integration\Framework\MockObject;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Entity\Plugin\DataType\EntityAdapter;
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\user\UserInterface;
@@ -52,11 +53,17 @@ trait MockDrupalEntityTrait {
     foreach ($fields as $field_name => $field) {
       $fields[$field_name] = $this->createFieldItemListMock($field);
       $fields[$field_name]->method('getName')->willReturn($field_name);
+      $fields[$field_name]->method('getEntity')->willReturn($mock_entity);
       $fields[$field_name]->method('getParent')->willReturn(
         $this->createConfiguredMock(EntityAdapter::class, [
           'getEntity' => $mock_entity,
         ])
       );
+
+      $field_definition = $this->createMock(FieldDefinitionInterface::class);
+      $field_definition->method('getName')->willReturn($field_name);
+      $fields[$field_name]->method('getFieldDefinition')
+        ->willReturn($field_definition);
     }
 
     // Make the field known to hasField.
@@ -92,6 +99,28 @@ trait MockDrupalEntityTrait {
     return $mock_entity;
   }
 
+  /**
+   * @param array[] $field_item_list_value
+   *   An array of item value arrays, e.g. "[['value' => 'bar'],['value' => 'baz']]"
+   *
+   * @return \Drupal\Core\Field\FieldItemListInterface
+   *
+   * IT'S USUALLY BETTER to do the following than to use this method directly,
+   * that way you get a more complete mock having parent and field definition
+   * methods available.
+   *
+   * @code
+   * $node = $this->createEntityMock('node', 'foo', [
+   *   'field_foo' => [
+   *     ['value' => 'bar'],
+   *     ['value' => 'baz'],
+   *   ],
+   * ]);
+   * $item_list = $node->get('field_foo');
+   * @endcode
+   *
+   * @see self::createEntityMock()
+   */
   public function createFieldItemListMock(array $field_item_list_value) {
     $field_item_list = $this->createMock(FieldItemListInterface::class);
     $field_item_list->method('getValue')->willReturn($field_item_list_value);
@@ -123,7 +152,8 @@ trait MockDrupalEntityTrait {
     };
     $field_item_list->method('__get')->willReturnCallback($callback);
     $field_item_list->method('get')->willReturnCallback($callback);
-    $field_item_list->method('count')->willReturn(count($field_item_list_value));
+    $field_item_list->method('count')
+      ->willReturn(count($field_item_list_value));
 
     (new MakeMockIterable())($field_item_list, $field_item_list_value);
 
